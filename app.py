a96d6c1b01c75604495887e4aa11cef7d3d1a90d
Gabriel <cloudgabriel@protonmail.com>
2022-07-31 03:45:26 -0300

    chore: add function that send a message if the message has an image

--- app.py
+++ app.py
@@ -10,4 +10,16 @@ app = discord.Client()
 async def on_ready():
   print("Bot esta online")
 
+@app.event
+async def on_message(message):
+  if message.author == app.user:
+    return
+  
+  # Check if message has an image
+  if not len(message.attachments) > 0:
+    return
+
+  if message.channel.id == 1003185959405375498:
+    await message.channel.send("Oi")
+
 app.run(os.getenv('BOT_TOKEN'))
