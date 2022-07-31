cb2ea33797cfeb6ee512d85f5df84a770a616fa6
Gabriel <cloudgabriel@protonmail.com>
2022-07-31 04:06:47 -0300

    chore: downloading image from message

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,14 +13,22 @@ async def on_ready():
 
 @app.event
 async def on_message(message):
-  if message.author == app.user:
-    return
-  
-  # Check if message has an image
-  if not len(message.attachments) > 0:
-    return
-
   if message.channel.id == 1003185959405375498:
-    await message.channel.send("Oi")
+    if message.author == app.user:
+      return
+    
+    # Check if message has an image
+    if not len(message.attachments) > 0:
+      return
 
+    try:
+      image = requests.get(message.attachments[0].url).content
+      with open('discord_image.png', 'wb') as handler:
+        handler.write(image)
+    except:
+        return await message.channel.send('Houve um erro ao tentar baixar a imagem')
+        
+    emote = '✅'
+    await message.add_reaction(emote)
+    
 app.run(os.getenv('BOT_TOKEN'))
